e782f98f5af44b719b0a365990ea24c357ae5856
Daniel Gioulakis <daniel@dgdev.net>
2012-09-20 16:40:13 -0400

    Added back IEnumerable support for IRTO.

--- NContext.Common/Extensions/IResponseTransferObjectIEnumerableExtensions.cs
+++ NContext.Common/Extensions/IResponseTransferObjectIEnumerableExtensions.cs
@@ -1,23 +1,3 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="IResponseTransferObjectIQueryableExtensions.cs" company="Waking Venture, Inc.">
-//   Copyright (c) 2012 Waking Venture, Inc.
-//
-//   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-//   documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
-//   the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
-//   and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-//
-//   The above copyright notice and this permission notice shall be included in all copies or substantial portions 
-//   of the Software.
-//
-//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
-//   TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-//   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-//   CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
-//   DEALINGS IN THE SOFTWARE.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
-
 namespace NContext.Extensions
 {
     using System;
@@ -27,18 +7,18 @@ namespace NContext.Extensions
     using NContext.Common;
 
     /// <summary>
-    /// Defines extension methods for <see cref="IQueryable{T}"/> yielding a new <see cref="IResponseTransferObject{T}"/>.
+    /// Defines extension methods for <see cref="IEnumerable{T}"/> yielding a new <see cref="IResponseTransferObject{T}"/>.
     /// </summary>
-    public static class IResponseTransferObjectIQueryableExtensions
+    public static class IResponseTransferObjectIEnumerableExtensions
     {
         /// <summary>
         /// Returns an <see cref="IResponseTransferObject{T}"/> with the first element of a sequence.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="queryable">The <see cref="IQueryable{T}"/> to return the first element of.</param>
+        /// <param name="queryable">The <see cref="IEnumerable{T}"/> to return the first element of.</param>
         /// <param name="predicate">An optional function to test each element for a condition.</param>
         /// <returns>IResponseTransferObject{T} with the first element in the sequence that passes the test in the (optional) predicate function.</returns>
-        public static IResponseTransferObject<T> FirstResponse<T>(this IQueryable<T> queryable, Func<T, Boolean> predicate = null)
+        public static IResponseTransferObject<T> FirstResponse<T>(this IEnumerable<T> queryable, Func<T, Boolean> predicate = null)
         {
             // TODO: (DG) Re-write this error!
             using (var enumerator = GetEnumerator(queryable, predicate))
@@ -56,10 +36,10 @@ namespace NContext.Extensions
         /// Returns an <see cref="IResponseTransferObject{T}"/> with the single, specific element of a sequence.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="querable">The <see cref="IQueryable{T}"/> to return the single element of.</param>
+        /// <param name="querable">The <see cref="IEnumerable{T}"/> to return the single element of.</param>
         /// <param name="predicate">An optional function to test each element for a condition.</param>
         /// <returns>IResponseTransferObject{T} with the single element in the sequence that passes the test in the (optional) predicate function.</returns>
-        public static IResponseTransferObject<T> SingleResponse<T>(this IQueryable<T> querable, Func<T, Boolean> predicate = null)
+        public static IResponseTransferObject<T> SingleResponse<T>(this IEnumerable<T> querable, Func<T, Boolean> predicate = null)
         {
             // TODO: (DG) Re-write these errors!
             using (var enumerator = GetEnumerator(querable, predicate))
@@ -79,7 +59,7 @@ namespace NContext.Extensions
             return new ServiceResponse<T>(new Error("MoreThanOneMatch", new[] { "More than one match!" }));
         }
 
-        private static IEnumerator<T> GetEnumerator<T>(IQueryable<T> queryable, Func<T, Boolean> predicate = null)
+        private static IEnumerator<T> GetEnumerator<T>(IEnumerable<T> queryable, Func<T, Boolean> predicate = null)
         {
             return (predicate == null) ? queryable.GetEnumerator() : queryable.Where(predicate).GetEnumerator();
         }
