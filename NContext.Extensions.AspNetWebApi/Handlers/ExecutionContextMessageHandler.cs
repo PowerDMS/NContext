b6b2576d4b917968df5ad9b4d7943e8561336339
Daniel Gioulakis <daniel@dgdev.net>
2013-04-12 15:14:41 -0400

    Added a new message handler for uniquely identifying an execution context.

---
+++ NContext.Extensions.AspNetWebApi/Handlers/ExecutionContextMessageHandler.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExecutionContextMessageHandler.cs" company="Waking Venture, Inc.">
+//   Copyright (c) 2013 Waking Venture, Inc.
+//
+//   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+//   documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+//   the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+//   and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//   The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+//   of the Software.
+//
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+//   TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+//   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+//   CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+//   DEALINGS IN THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace NContext.Extensions.AspNetWebApi.Handlers
+{
+    using System.Net.Http;
+    using System.Runtime.Remoting.Messaging;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Defines a message handler which creates an identifier for the incoming ExecutionContext flow.
+    /// This can be later used across asyncronous operations and threads to uniquely identify the execution context.
+    /// This may be useful if you wish to cache data per-request (HttpContext.Current.Items). Since a single 
+    /// request/response may execute across multiple threads we need to identify the data somehow. This is similar to how
+    /// Thread.CurrentPrincipal and Culture work.
+    /// </summary>
+    public class ExecutionContextMessageHandler : DelegatingHandler
+    {
+        /// <summary>
+        /// Sends an HTTP request to the inner handler to send to the server as an asynchronous operation.
+        /// </summary>
+        /// <param name="request">The HTTP request message to send to the server.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel operation.</param>
+        /// <returns>Returns <see cref="T:System.Threading.Tasks.Task`1" />. The task object representing the asynchronous operation.</returns>
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            CallContext.LogicalSetData("ecid", request.GetCorrelationId().ToString());
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
