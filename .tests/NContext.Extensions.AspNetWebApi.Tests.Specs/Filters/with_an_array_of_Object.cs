7deea639854807622cbeb36fd254995fa3bd8268
Daniel Gioulakis <daniel@dgdev.net>
2014-01-17 22:19:12 -0500

    added new files for the previous commit

---
+++ .tests/NContext.Extensions.AspNetWebApi.Tests.Specs/Filters/with_an_array_of_Object.cs
@@ -0,0 +1,40 @@
+﻿namespace NContext.Extensions.AspNetWebApi.Tests.Specs.Filters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    using Machine.Specifications;
+
+    using NContext.Text;
+
+    using Telerik.JustMock;
+
+    public class with_an_array_of_Object : when_sanitizing_objects_with_ObjectGraphSanitizer
+    {
+        Establish context = () =>
+            {
+                TextSanitizer = Mock.Create<ISanitizeText>();
+
+                Mock.Arrange(() => TextSanitizer.SanitizeHtmlFragment(Arg.AnyString))
+                    .Returns(_SanitizedValue);
+
+                _Data = new Object[]
+                    {
+                        5,
+                        "Daniel",
+                        null,
+                        "Gioulakis",
+                        ""
+                    };
+            };
+
+        Because of = () => Sanitize(_Data);
+
+        It should_sanitize_the_strings = () => _Data.ShouldContainOnly(5, _SanitizedValue, null, _SanitizedValue, "");
+
+        static IEnumerable<Object> _Data;
+
+        const String _SanitizedValue = "ncontext";
+    }
+}
---
+++ .tests/NContext.Extensions.AspNetWebApi.Tests.Specs/Filters/with_an_array_of_String.cs
@@ -0,0 +1,36 @@
+﻿namespace NContext.Extensions.AspNetWebApi.Tests.Specs.Filters
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Machine.Specifications;
+
+    using NContext.Text;
+
+    using Telerik.JustMock;
+
+    public class with_an_array_of_String : when_sanitizing_objects_with_ObjectGraphSanitizer
+    {
+        Establish context = () =>
+            {
+                TextSanitizer = Mock.Create<ISanitizeText>();
+
+                Mock.Arrange(() => TextSanitizer.SanitizeHtmlFragment(Arg.AnyString))
+                    .Returns(_SanitizedValue);
+
+                _Data = new String[]
+                    {
+                        "Daniel",
+                        "Gioulakis"
+                    };
+            };
+
+        Because of = () => Sanitize(_Data);
+
+        It should_sanitize_the_enumerable = () => _Data.ShouldContainOnly(_SanitizedValue, _SanitizedValue);
+
+        static IEnumerable<String> _Data;
+
+        const String _SanitizedValue = "ncontext";
+    }
+}
